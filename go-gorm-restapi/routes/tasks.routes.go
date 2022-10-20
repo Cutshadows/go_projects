592da2ed3837e1876c0580c1116a478c8e96b2a5
douglas_barraza <douglas.barraza@externos-cl.cencosud.com>
2022-10-20 09:47:56 -0300

    feat: task routes modules

--- go-gorm-restapi/routes/tasks.routes.go
+++ go-gorm-restapi/routes/tasks.routes.go
@@ -1,19 +1,67 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/Cutshadows/go-gorm-restapi/db"
+	"github.com/Cutshadows/go-gorm-restapi/models"
+	"github.com/gorilla/mux"
+)
 
 func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world Tasks"))
+	var tasks []models.Task
+	db.DB.Find(&tasks)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world Tasks"))
+	var task models.Task
+	params := mux.Vars(r)
+	taskId, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Id not provider"))
+		return
+	}
+	db.DB.First(&task, taskId)
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
 }
 
 func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world Tasks"))
+	var task models.Task
+	json.NewDecoder(r.Body).Decode(&task)
+	createTask := db.DB.Create(&task)
+	err := createTask.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		w.Write([]byte(err.Error()))
+	}
+	json.NewEncoder(w).Encode(&task)
 }
 
 func DeletetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world Tasks"))
+	var task models.User
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	db.DB.Delete(&task) // no elimina de postgress solo le agrega una fecha de eliminacion
+	//db.DB.Unscoped().Delete(&task)
+	w.WriteHeader(http.StatusOK)
 }
--- go-gorm-restapi/routes/users.routes.go
+++ go-gorm-restapi/routes/users.routes.go
@@ -3,17 +3,41 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/Cutshadows/go-gorm-restapi/db"
 	"github.com/Cutshadows/go-gorm-restapi/models"
+	"github.com/gorilla/mux"
 )
 
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world users"))
+	var users []models.User
+	db.DB.Find(&users)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
 
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world users"))
+	var user models.User
+	params := mux.Vars(r)
+	idUser, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Id not provider"))
+		return
+	}
+	db.DB.First(&user, idUser)
+
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found"))
+		return
+	}
+
+	db.DB.Model(&user).Association("Tasks").Find(&user.Tasks)
+
+	json.NewEncoder(w).Encode(&user)
 }
 
 func PostUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,5 +53,17 @@ func PostUserHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeletetUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world users"))
+	var user models.User
+	params := mux.Vars(r)
+	db.DB.First(&user, params["id"])
+
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found"))
+		return
+	}
+
+	//db.DB.Delete(&user) // no elimina de postgress solo le agrega una fecha de eliminacion
+	db.DB.Unscoped().Delete(&user)
+	w.WriteHeader(http.StatusOK)
 }
